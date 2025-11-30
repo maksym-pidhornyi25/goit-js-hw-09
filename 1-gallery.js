e23dc358a6111030e548663c22ac2a47c52ed033
maksym-pidhornyi25 <maksym.ldc@gmail.com>
2025-11-30 17:12:14 +0000

    Deploying to gh-pages from @ maksym-pidhornyi25/goit-js-hw-09@f79f5550dc6555963507e4b7753c1fb021a6da60 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */document.addEventListener("DOMContentLoaded",()=>{console.log("JS працює!")});
+//# sourceMappingURL=1-gallery.js.map
